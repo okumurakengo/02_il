e1f700247b441a4b948ed110f63d937f2f503b1a
okumurakengo <okumurakengo1a@gmail.com>
2018-06-14 22:29:09 +0900

    LuckyController.php test

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/lucky")
+ */
+class LuckyController
+{
+    /**
+     * @Route("/number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
